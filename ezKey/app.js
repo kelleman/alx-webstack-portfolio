1e552cf3a2ed5462735427f59d141f24cb8fb276
kelleman <kellemanatser@gmail.com>
2023-10-15 20:26:53 +0100

    .

---
+++ ezKey/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const db = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.use('/api/v1', userRoutes);
+app.set('view engine', 'ejs')
+
+// routes prefix
+app.use('/', require('./routes/apiRoutes'));
+
+
+const port = process.env.PORT || 3030;
+app.listen(port, ()=>{
+    console.log(`listening to: http://localhost:${port}`)
+})
