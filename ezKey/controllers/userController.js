4171633c2d227e06f7fbd2bdf15898dd61703af6
kelleman <kellemanatser@gmail.com>
2023-11-02 16:42:20 +0100

    .

--- ezKey/controllers/userController.js
+++ ezKey/controllers/userController.js
@@ -1,8 +1,12 @@
-
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const sendEmail = require('../utils/sendEmail')
+const { generateOTP,sendOTP,sendNewOTP } = require('../utils/sendEmail'); 
 
+const uuid = require('uuid');
+
+// user registration logic
 exports.register = async (req, res) => {
   try {
     const { firstname, lastname, email, phone, username, password } = req.body;
@@ -16,12 +20,27 @@ exports.register = async (req, res) => {
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 8);
 
-    // Create a new user
-    const newUser = new User({ firstname, lastname, email, phone, username, password: hashedPassword });
+    // Generate a random OTP
+    const otp = generateOTP();
+
+   
+    const newUser = new User({
+      firstname,
+      lastname,
+      email,
+      phone,
+      username,
+      password: hashedPassword,
+      otp, 
+    });
+
     const savedUser = await newUser.save();
 
+    // Send the OTP to the user's email
+    sendOTP(email, otp);
+
     res.status(201).json({
-      message: 'Registration successful',
+      message: 'Registration successful. Please check your email for the OTP.',
       user: {
         _id: savedUser._id,
         firstname: savedUser.firstname,
@@ -29,7 +48,6 @@ exports.register = async (req, res) => {
         email: savedUser.email,
         phone: savedUser.phone,
         username: savedUser.username,
-        // Include other user details you want to return
       },
     });
   } catch (error) {
@@ -38,7 +56,67 @@ exports.register = async (req, res) => {
   }
 };
 
+// verify OTP logic
+exports.verifyOTP = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
 
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (user.otp !== otp) {
+      return res.status(400).json({ message: 'Invalid OTP' });
+    }
+
+    // Mark the user as verified
+    user.isVerified = true;
+    await user.save();
+
+    res.status(200).json({ message: 'OTP verified successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// resend OTP logic
+exports.resendOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Check if the user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Generate a new OTP
+    const newOTP = sendEmail.generateOTP();
+
+    // Update the OTP in the user document
+    user.otp = newOTP;
+
+    // Save the updated user document to the database
+    await user.save();
+
+    // Send the new OTP via email using the sendEmail module
+    sendEmail.sendNewOTP(user.email, newOTP);
+
+    res.status(200).json({ message: 'New OTP sent to your email' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+
+
+//login logic
 exports.login = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -47,14 +125,19 @@ exports.login = async (req, res) => {
     const user = await User.findOne({ username });
 
     if (!user) {
-      return res.status(401).json({ message: 'User not exist' });
+      return res.status(401).json({ message: 'User does not exist' });
+    }
+
+    // Check if the user is verified
+    if (!user.isVerified) {
+      return res.status(401).json({ message: 'You are not yet verified' });
     }
 
     // Compare the password
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'wrong Credentials' });
+      return res.status(401).json({ message: 'Wrong Credentials' });
     }
 
     // Generate a token
@@ -62,12 +145,75 @@ exports.login = async (req, res) => {
       expiresIn: '1h',
     });
     
-    res.status(200).json({message:'Logged in successfully', token });
+    res.status(200).json({ message: 'Logged in successfully', token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
   }
 };
 
- 
+// reset password request logic
+exports.resetPasswordRequest = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Check if the user exists based on the provided email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Generate a new OTP
+    const newOTP = sendEmail.generateOTP();
+
+    // Update the user's record in the database with the new OTP
+    user.otp = newOTP;
+    await user.save();
+
+    // Send the new OTP via email
+    sendEmail.sendNewOTP(user.email, newOTP);
+
+    res.status(200).json({ message: 'New OTP sent to your email' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+
+exports.resetPasswordVerify = async (req, res) => {
+  try {
+    const { username, otp, newPassword } = req.body;
+    const user = await User.findOne({ $or: [{ username }, { email: username }] });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (user.otp !== otp) {
+      return res.status(401).json({ message: 'Invalid OTP' });
+    }
+
+    // OTP is valid, update the user's password
+    // const saltRounds = 10; 
+    const hashedPassword = await bcrypt.hash(newPassword, 10); // 10 rounds of salt
+
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({ message: 'Password updated successfully' });
+    const newOTP = generateOTP();
+
+//user.otp = null to clear it from the document
+user.otp = null; 
+
+// Save the user document with the OTP  null value
+await user.save();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
 
--- ezKey/models/userModel.js
+++ ezKey/models/userModel.js
@@ -10,34 +10,41 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: [true, "password length mus be 8 characters and above"],
-  },
-  phone: { 
-    type: String,
-    required: true
-  },
-  // role: {
-  //   type: String,
-  //   enum: ['Inhabitant', 'Visitor', 'Admin'],
-  //   default: 'Visitor'
-  // },
-  
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password length must be 8 characters and above"],
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: Boolean, 
+        default: false 
+    },
+    otp: {
+        type: String,
+      },
+      isVerified: {
+        type: Boolean,  
+        default: false, 
+      },
+      resetPasswordOTP: {
+        type: String
+      },
 }, {
-  timestamps: true 
+    timestamps: true
 });
-
 
 const User = mongoose.model('User', userSchema);
 
